da2d3b38a442824a0df529142ed3cb9c0da46157
CaptainZiboo <96137708+CaptainZiboo@users.noreply.github.com>
2023-12-01 15:20:11 +0100

    Delete build directory
    removed build dir

--- build/utils/esilv.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SupportedFormations = exports.SupportedYears = void 0;
-exports.SupportedYears = ["A3"];
-exports.SupportedFormations = ["ALTERNANCE"];
